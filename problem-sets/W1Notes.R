59a2ca64806098d87bb9e33334ce64931fb53053
flo8001 <florenceli@ucla.edu>
2025-10-04 12:57:19 -0700

    This is my first independent commit!

--- problem-sets/W1Notes.R
+++ problem-sets/W1Notes.R
@@ -12,3 +12,4 @@ gitcreds_set()
 
 #Note1 could not install usethis b/c R was outdated, not RStudio. 
 #Note2 did not have permission to "push" files b/c didn't use URL associated with my GitHub account. Had to delete project and put it in Downloads instead of Desktop for R to remake it
+#
